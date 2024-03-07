72f40a7cac24506195589581efbb7abaf87ea8e8
João Augusto Lissoni Lanjoni <joaoaugustolanjoni@gmail.com>
2024-03-06 23:15:02 -0300

    feat: add xtdb support for postgres

--- src/com/emerauth/core.clj
+++ src/com/emerauth/core.clj
@@ -2,12 +2,14 @@
   (:require
    [com.emerauth.components.config :as config]
    [com.emerauth.components.logger :as logger]
+   [com.emerauth.components.db :as db]
    [com.stuartsierra.component :as component]))
 
 (def system-map
   (component/system-map
    :config (config/new-config "config.prod.edn")
-   :logger (component/using (logger/new-logger) [:config])))
+   :logger (component/using (logger/new-logger) [:config])
+   :db (component/using (db/new-db) [:config])))
 
 (defn -main
   "Emerauth's entry point."
